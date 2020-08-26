905d91eac7093584d6146c2c1fa3ff3d96a2c8f4
Сергей <nyos@rambler.ru>
2020-08-26 13:48:43 +0500

    0,0,5

---
+++ 1/micro-service/api-restoreCTN.php
@@ -0,0 +1,191 @@
+<?php
+
+ob_start('ob_gzhandler');
+
+try {
+
+
+//    $date = $in['date'] ?? $_REQUEST['date'] ?? null;
+//
+    if (empty($_REQUEST['phone']))
+        throw new \Exception('нет телефона');
+
+    if (isset($skip_start) && $skip_start === true) {
+        
+    } else {
+        require_once '0start.php';
+    }
+
+
+    // \f\pa($_REQUEST);
+
+
+    if (empty(\Nyos\api\Beeline::$token)) {
+
+
+        \Nyos\mod\items::$type_module = 3;
+        \Nyos\mod\items::$search['id'] = $_REQUEST['ban'];
+        $ee = \Nyos\mod\items::get($db, '701.beeline_dogovors');
+        
+        if (empty($ee[0]))
+            throw new \Exception('нет');
+
+        // \f\pa($ee);
+        \Nyos\api\Beeline::getToken($db, $ee[0]['login'], $ee[0]['pass']);
+        // \Nyos\api\Beeline::getToken($db);
+        
+    }
+
+    $c = new \SoapClient('https://my.beeline.ru/api/SubscriberService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
+    $opt = [
+        'token' => \Nyos\api\Beeline::$token,
+        'ctn' => $_REQUEST['phone'],
+        'actvDate' => date('Y-m-d\T00:00:00.000', $_SERVER['REQUEST_TIME']), // 2013-04-26T00:00:00.000<
+        'reasonCode' => 'RSBO',
+    ];
+    // \f\pa($opt);
+    $response = $c->restoreCTN($opt);
+    \f\pa($response);
+
+    \Nyos\mod\items::add( $db, '701.beeline_requests', [ 
+        'dogovor' =>  $_REQUEST['ban'],
+        'phone' => $_REQUEST['phone'],
+        'requestId' => $response->return 
+        ] );
+
+//    
+//        $c = new \SoapClient('https://my.beeline.ru/api/SubscriberService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
+//        $response = $c->auth(['login' => self::$login, 'password' => self::$pass ]);
+    // \f\pa($response);
+    // \f\pa($c->__getLastRequestHeaders());
+    // \f\pa($c->__getLastRequest());
+    // if (!empty($response->return)) {
+//    
+
+    $r = ob_get_contents();
+    ob_end_clean();
+
+    \f\end2($r, true);
+    \f\end2('(обработано) номеров в договоре 1', true);
+
+
+    if (\Nyos\Nyos::checkSecret($_REQUEST['s'], $_REQUEST['id'])) {
+        
+    } else {
+        throw new \Exception('что то пошло не так, повторите и обратитесь в тех. поддержку', false);
+    }
+
+    \Nyos\mod\items::$search['who_added'] = $_SESSION['now_user_di']['id'];
+    \Nyos\mod\items::$search['id'] = $_REQUEST['id'];
+    \Nyos\mod\items::$type_module = 3;
+    $dogs = \Nyos\mod\items::get($db, '701.beeline_dogovors');
+
+    // \f\end2('asd',true,$dogs);
+
+    if (!empty($dogs)) {
+        foreach ($dogs as $v) {
+
+            if (empty($v['login']) || empty($v['pass']))
+                throw new \Exception('что то пошло не так #' . __LINE__ . ', повторите и обратитесь в тех. поддержку', false);
+
+            \Nyos\api\Beeline::$login = trim('A' . $v['login']);
+            \Nyos\api\Beeline::$ban = trim($v['login']);
+            \Nyos\api\Beeline::$pass = trim($v['pass']);
+
+            break;
+        }
+    }
+
+//    $list = \Nyos\api\Beeline::getToten($db);
+//    \f\pa($list);
+//    \f\end2('23');
+    $list = (array) \Nyos\api\Beeline::getListNumber($db);
+    // \f\pa( $list );
+    // \f\pa((array) $list['CTNInfoList']);
+    $res = (array) $list['CTNInfoList'];
+
+//    \f\pa(sizeof( $res ));
+// \f\pa( $res );
+    // if ( sizeof( $res ) == 8 ) {
+
+    \Nyos\mod\Bibi_v1::deletePhonesFromDogovor($db, (int) $_REQUEST['id']);
+
+
+
+    \Nyos\mod\items::$type_module = 3;
+    // \f\pa( (array) $list['CTNInfoList']);
+    $res['dogovor_id'] = $_REQUEST['id'];
+    \Nyos\mod\items::add($db, \Nyos\mod\Bibi_v1::$mod_phones, $res);
+
+    \f\end2('(обработано) номеров в договоре 1', true);
+
+    // }
+
+
+    \f\end2('end', false);
+//
+//
+//    // \f\pa($_REQUEST);
+////    require_once( $_SERVER['DOCUMENT_ROOT'] . DS . '0.all' . DS . 'f' . DS . 'db.2.php' );
+////    require_once( $_SERVER['DOCUMENT_ROOT'] . DS . '0.all' . DS . 'f' . DS . 'txt.2.php' );
+//// $_SESSION['status1'] = true;
+//// $status = '';
+//
+//    $e = array('id' => (int) $_POST['item_id']);
+//
+//    $ff = $db->prepare('DELETE FROM `mitems-dops` WHERE `id_item` = :id_item AND `name` = :name ');
+//    $ff->execute(
+//            array(
+//                ':id_item' => $_POST['item_id'],
+//                ':name' => $_POST['dop_name']
+//            )
+//    );
+//
+//
+//    if (isset($_POST['new_val'])) {
+//        $sql = 'INSERT INTO `mitems-dops` ( `id_item`, `name`, `value` ) values ( :id, :name , :val ) ';
+//        // \f\pa($sql);
+//        $ff = $db->prepare($sql);
+//        $in_sql = [
+//            ':id' => $_POST['item_id'],
+//            ':name' => $_POST['dop_name'],
+//            ':val' => $_POST['new_val'] ?? 0,
+//        ];
+//        // \f\pa($in_sql);
+//        $ff->execute($in_sql);
+//    }
+//
+////    $dir_for_cash = DR . dir_site;
+////    $list_cash = scandir($dir_for_cash);
+////    foreach ($list_cash as $k => $v) {
+////        if (strpos($v, 'cash.items.') !== false) {
+////            unlink($dir_for_cash . $v);
+////        }
+////    }
+//// f\end2( 'новый статус ' . $status);
+//    f\end2('ок');
+} catch (\Exception $exc) {
+
+    \f\pa($exc);
+
+    $msg = '';
+
+    $r = ob_get_contents();
+    ob_end_clean();
+
+    \nyos\Msg::sendTelegramm($r, null, 2);
+
+    \f\end2($r, false);
+
+    if (isset($exc->detail->UssWsApiException->errorCode))
+        $msg .= ' ошибка #' . $exc->detail->UssWsApiException->errorCode . ' / ';
+
+    if (isset($exc->detail->UssWsApiException->errorDescription))
+        $msg .= $exc->detail->UssWsApiException->errorDescription;
+
+    // \f\end2($msg . ' ( #' . $exc->getCode() . ' ' . $exc->getMessage() . ' )', false, (array) $exc);
+//    echo '<pre>';
+//    print_r($exc);
+//    echo '</pre>';
+    // echo $exc->getTraceAsString();
+}
--- class.php
+++ class.php
@@ -6,8 +6,8 @@
 
 namespace Nyos\api;
 
-if (!defined('IN_NYOS_PROJECT'))
-    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
+//if (!defined('IN_NYOS_PROJECT'))
+//    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
 
 class Beeline {
 
@@ -20,29 +20,40 @@ class Beeline {
         
     }
 
-    static public function getToten($db, $login = '', $pass = '') {
+    static public function getToken($db, $login = '', $pass = '') {
 
         $cash_var = 'bee_token';
 
         if (!empty($cash_var)) {
+
             $vv = \f\Cash::getVar($cash_var);
+
             if (!empty($vv)) {
                 self::$token = $vv;
                 // echo '<br/>#'.__LINE__;
                 // return $vv;
                 return \f\end3('ok', true, ['cash' => true, 'token' => $vv]);
             }
+
         }
 
-        $c = new SoapClient('https://my.beeline.ru/api/AuthService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
+        if( empty(self::$login) )
+            throw new \Exception ('нет логина');
+        
+        $c = new \SoapClient('https://my.beeline.ru/api/AuthService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
         $response = $c->auth(['login' => self::$login, 'password' => self::$pass ]);
         // \f\pa($response);
         // \f\pa($c->__getLastRequestHeaders());
         // \f\pa($c->__getLastRequest());
         if (!empty($response->return)) {
-            \f\Cash::setVar('bee_token', $response->return);
+            
+            if (!empty($cash_var))
+            \f\Cash::setVar( $cash_var , $response->return, 6200 );
+            
             self::$token = $response->return;
+            
             return \f\end3('ok', true, ['cash' => false, 'token' => $response->return]);
+            
         } else {
             return false;
         }
@@ -50,19 +61,25 @@ class Beeline {
 
     static public function getListNumber($db) {
 
+        self::getToken($db);
+        
         if( empty(self::$token) )
-            if( empty(self::getToten($db)) ){
+            if( empty(self::getToken($db)) ){
                 throw new \Exception( 'нет токена' );
             }
         
-        $c = new SoapClient('https://my.beeline.ru/api/SubscriberService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
+            // \f\pa(self::$token);
+            
+        $c = new \SoapClient('https://my.beeline.ru/api/SubscriberService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
         $response = $c->getCTNInfoList([ 'token'=>self::$token, 'ban' => self::$ban ]);
-        \f\pa($response);
+        // \f\pa($response);
         
         // \f\pa($c->__getLastRequestHeaders());
         // \f\pa($c->__getLastRequest());
 
         // https://my.beeline.ru/api/SubscriberService?WSDL
+        
+        return $response;
     }
 
 }
