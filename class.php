c17d9f521c9dc534113450bcb01975deb17c1c4e
nyosru <nyos@rambler.ru>
2021-02-24 17:32:26 +0500

    0.1.0

--- class.php
+++ class.php
@@ -1,5 +1,7 @@
 <?php
 
+//use \SoapClient;
+
 /**
   класс модуля
  * */
@@ -9,18 +11,73 @@ namespace Nyos\api;
 //if (!defined('IN_NYOS_PROJECT'))
 //    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
 
-class Beeline {
+class Beeline
+{
 
     public static $token = [];
     public static $login = '';
     public static $pass = '';
     public static $ban = '';
 
-    function __construct() {
-        
+    // function __construct() {
+    // }
+
+    static public function lara__getToken($login = '', $pass = '')
+    {
+
+        if (!empty($login))
+            self::$login = $login;
+
+        if (!empty($pass))
+            self::$pass = $pass;
+
+        try {
+
+            //code...
+            $c = new \SoapClient('https://my.beeline.ru/api/AuthService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
+
+            dd( [ 
+                $c->__getTypes() , '111',
+                // $c->__getFunctions() , '222',
+                // // $c->__call() , 
+                // // $c->__doRequest() , 
+                // $c->__getFunctions(), ' - Returns list of SOAP functions' , 
+                $c->__getLastRequest(), ' - Returns last SOAP request ' , 
+                // $c->__getLastRequestHeaders() , ' - Returns last SOAP request headers ' , 
+                $c->__getLastResponse()  , ' - Returns last SOAP response ' , 
+                // $c->__getLastResponseHeaders() , ' - Returns last SOAP response headers' , 
+                // $c->__getTypes(), ' - Returns list of SOAP types ' , 
+                // $c->__setCookie() , ' - Sets the cookie that will be sent with the SOAP request ' , 
+                // $c->__soapCall() , ' - Calls a SOAP function ' ,
+            
+                self::$login, 
+                self::$pass 
+                ] );
+
+            $response = $c->auth(['login' => self::$login, 'password' => self::$pass]);
+
+
+        } catch (\Exception $ex) {
+
+            // dd($ex);
+            $response = $ex;
+
+        } 
+        // catch (\Throwable $th) {
+        //     //throw $th;
+        // }
+
+
+        return [ 
+            ( $response ?? 'x' ) , 
+            [ 'login' => self::$login, 'pass' => self::$pass] 
+        ];
     }
 
-    static public function getToken($db, $login = '', $pass = '') {
+
+
+    static public function getToken($db, $login = '', $pass = '')
+    {
 
         $cash_var = 'bee_token';
 
@@ -34,52 +91,50 @@ class Beeline {
                 // return $vv;
                 return \f\end3('ok', true, ['cash' => true, 'token' => $vv]);
             }
-
         }
 
-        if( empty(self::$login) )
-            throw new \Exception ('нет логина');
-        
+        if (empty(self::$login))
+            throw new \Exception('нет логина');
+
         $c = new \SoapClient('https://my.beeline.ru/api/AuthService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
-        $response = $c->auth(['login' => self::$login, 'password' => self::$pass ]);
+        $response = $c->auth(['login' => self::$login, 'password' => self::$pass]);
         // \f\pa($response);
         // \f\pa($c->__getLastRequestHeaders());
         // \f\pa($c->__getLastRequest());
         if (!empty($response->return)) {
-            
+
             if (!empty($cash_var))
-            \f\Cash::setVar( $cash_var , $response->return, 6200 );
-            
+                \f\Cash::setVar($cash_var, $response->return, 6200);
+
             self::$token = $response->return;
-            
+
             return \f\end3('ok', true, ['cash' => false, 'token' => $response->return]);
-            
         } else {
             return false;
         }
     }
 
-    static public function getListNumber($db) {
+    static public function getListNumber($db)
+    {
 
         self::getToken($db);
-        
-        if( empty(self::$token) )
-            if( empty(self::getToken($db)) ){
-                throw new \Exception( 'нет токена' );
+
+        if (empty(self::$token))
+            if (empty(self::getToken($db))) {
+                throw new \Exception('нет токена');
             }
-        
-            // \f\pa(self::$token);
-            
+
+        // \f\pa(self::$token);
+
         $c = new \SoapClient('https://my.beeline.ru/api/SubscriberService?WSDL', array('trace' => false, 'cache_wsdl' => WSDL_CACHE_NONE));
-        $response = $c->getCTNInfoList([ 'token'=>self::$token, 'ban' => self::$ban ]);
+        $response = $c->getCTNInfoList(['token' => self::$token, 'ban' => self::$ban]);
         // \f\pa($response);
-        
+
         // \f\pa($c->__getLastRequestHeaders());
         // \f\pa($c->__getLastRequest());
 
         // https://my.beeline.ru/api/SubscriberService?WSDL
-        
+
         return $response;
     }
-
 }
